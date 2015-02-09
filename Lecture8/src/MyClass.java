0d0e94e72f621ebf645b7f2d47b8978f162540ed
hamedika <hamedika@ROSA02.ad.arcada.fi>
2015-02-09 12:13:58 +0200

    Fixat

--- Lecture8/src/MyClass.java
+++ Lecture8/src/MyClass.java
@@ -2,15 +2,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+public class MyClass implements MyInterface {
+	MyInterface my;
+	String text = "";
+	public MyClass() {
+		this.my = new FakeClass();
+		text = my.readFromKeyboard();
+	}
+	public MyClass(MyInterface my) {
+		this.my = my;
+	}
 
-public class MyClass {
-	public void readFromKeyboard(){
-		String text;
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		try{
-			text = br.readLine();
-		} catch (IOException e){
-			e.printStackTrace();
+	public class FakeClass implements MyInterface {
+		@Override
+		public String readFromKeyboard() {
+			return "test";
 		}
+	}
+	public class ValidReader implements MyInterface {
+		public String readFromKeyboard() {
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					System.in));
+			try {
+				text = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return text;
+		}
+	}
+
+	@Override
+	public String readFromKeyboard() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
