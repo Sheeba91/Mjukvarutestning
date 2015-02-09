b02bced9023aa7e41769fbe483d53fc17c519168
forsstho <forsstho@ROSA21.ad.arcada.fi>
2015-02-09 12:12:11 +0200

    Fixed unit test so it now works, also added interface

--- Lecture8/src/MyClass.java
+++ Lecture8/src/MyClass.java
@@ -4,13 +4,33 @@ import java.io.InputStreamReader;
 
 
 public class MyClass {
-	public void readFromKeyboard(){
-		String text;
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		try{
-			text = br.readLine();
-		} catch (IOException e){
-			e.printStackTrace();
+	MyInterface my;
+	String text = "";
+	public MyClass(){ 
+		this.my = new FakeClass();
+		text = my.readFromKeyboard();
+	}
+	public MyClass(MyInterface my) {
+		this.my = my;
+		text = my.readFromKeyboard();
+	}
+	public class FakeClass implements MyInterface {
+		@Override
+		public String readFromKeyboard() {
+			return "test";
+		}		
+	}
+	public class ValidReader implements MyInterface {
+		public String readFromKeyboard(){
+			
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(System.in));
+			try{
+				text = br.readLine();
+			} catch (IOException e){
+				e.printStackTrace();
+			}
+			return text;
 		}
 	}
 }
